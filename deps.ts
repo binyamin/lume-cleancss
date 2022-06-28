0fdfdf73fcfa6cb3dd61fade3c5c9f4b9a9e60f8
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-06-27 20:08:27 -0400

    Add logic

---
+++ deps.ts
@@ -0,0 +1,5 @@
+export * as path from 'https://deno.land/std@0.145.0/path/mod.ts';
+export type { Site } from 'https://deno.land/x/lume@v1.10.0/core.ts';
+export { Page } from 'https://deno.land/x/lume@v1.10.0/core/filesystem.ts';
+export { default as CleanCSS } from 'https://esm.sh/clean-css@5.3.0';
+
