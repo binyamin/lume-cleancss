c508616ef834cb80990914361358d8be19308cab
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-07-13 13:04:03 -0400

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as path from 'https://deno.land/std@0.145.0/path/mod.ts';
+export * as path from 'https://deno.land/std@0.148.0/path/mod.ts';
 export type { Site } from 'https://deno.land/x/lume@v1.10.0/core.ts';
 export { Page } from 'https://deno.land/x/lume@v1.10.0/core/filesystem.ts';
-export { default as CleanCSS } from 'https://esm.sh/clean-css@5.3.0';
+export { default as CleanCSS } from 'https://esm.sh/clean-css@5.3.1';
